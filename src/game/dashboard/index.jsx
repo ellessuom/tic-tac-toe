c420fc7b7ebc66ea1c60e1c39e41c0201d243ab0
Demousselle <dev@offon.dev>
2020-11-28 16:06:43 +0000

    Adds base structure to Dashboard component

--- src/game/dashboard/index.jsx
+++ src/game/dashboard/index.jsx
@@ -1,5 +1,15 @@
-export default function Dashboard() {
+import React from 'react';
+
+import Status from './components/status';
+import Matrix from './components/matrix';
+import Footer from './components/footer';
+
+export default function Dashboard () {
   return (
-    <div>Dashboard</div>
-  )
+    <div>
+      <Status />
+      <Matrix />
+      <Footer />
+    </div>
+  );
 }
