5a2e3e1d423c4c70fe18da5f4969d245869aa073
Demousselle <dev@offon.dev>
2020-11-28 16:04:24 +0000

    Renames main file from 'Game.js' to 'index.js'

--- src/game/index.js
+++ src/game/index.js
@@ -1,1 +1,16 @@
-export { default } from './Game';
+import React from 'react';
+import Layout from './layout';
+import Dashboard from './dashboard';
+import Players from './players';
+
+function Index() {
+    return (
+      <Layout>
+        <Players />
+        <Dashboard />
+      </Layout>
+    );
+}
+
+
+export default Index;
