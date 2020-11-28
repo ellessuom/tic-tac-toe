7a34d9b9be18fa16d28d28f0f3788d5edf07d48c
Demousselle <dev@offon.dev>
2020-11-28 16:00:57 +0000

    Adds base styling to base-player and players components including common SCSS variables

--- src/game/players/components/base-player/index.jsx
+++ src/game/players/components/base-player/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import './styles.css';
+
 export default function BasePlayer ({ id }) {
   return (
-    <div>
+    <div className={`base-player-display ${id}`}>
       <h1>Player {id}</h1>
       <p>Wins: {0}</p>
     </div>
--- src/game/players/index.jsx
+++ src/game/players/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Player from './components/base-player';
 
+import './styles.css';
 export default function Players () {
   return (
-    <div>
+    <div className="players-wrapper">
       <Player id="one" />
       <Player id="two" />
     </div>
