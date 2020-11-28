bf1b4222fbf9e2eb4226b844a12666d9c78d15c5
Demousselle <dev@offon.dev>
2020-11-28 15:36:57 +0000

    Removes boilerplate and renames folders to follow common naming conventions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import Game from './Game';
+import Game from './game';
 
-ReactDOM.render(<Game />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
