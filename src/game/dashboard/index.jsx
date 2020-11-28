2f63a9003d41550c380da4190d2e0e6775accd60
Demousselle <dev@offon.dev>
2020-11-28 16:10:22 +0000

    Adds base styling to dashboard component

--- src/game/dashboard/index.jsx
+++ src/game/dashboard/index.jsx
@@ -4,9 +4,10 @@ import Status from './components/status';
 import Matrix from './components/matrix';
 import Footer from './components/footer';
 
+import './styles.css';
 export default function Dashboard () {
   return (
-    <div>
+    <div className="container">
       <Status />
       <Matrix />
       <Footer />
